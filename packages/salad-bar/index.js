d6439897058ab915441eb443117ad66d834aa779
Will King <contact@wking.io>
2018-01-23 15:02:42 -0600

    Adding master package

---
+++ packages/salad-bar/index.js
@@ -0,0 +1,11 @@
+import addClass from 'saladbar.addclass';
+import hasClass from 'saladbar.hasclass';
+import removeClass from 'saladbar.removeclass';
+import toggleClass from 'saladbar.toggleclass';
+
+export default {
+  addClass,
+  hasClass,
+  removeClass,
+  toggleClass,
+};
