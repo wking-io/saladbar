92d67e9f3f50aeaa5f2d204d2ad892f7f234eb73
Will King <contact@wking.io>
2018-02-03 09:52:24 -0600

    Updating rollup config to split the iife bundle out so new commit is not needed after publishing to npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 /* eslint sort-keys: 0 */
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import includePaths from 'rollup-plugin-includepaths';
-import node from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
@@ -18,38 +16,7 @@ const babelrc = {
   plugins: ['external-helpers'],
 };
 
-const banner = `/**
- * salad-bar bundled; version ${pkg.version}
- */
-`;
-
 export default [
-  {
-    input: 'src/index.js',
-    output: {
-      banner,
-      file: 'lib/bundle.js',
-      format: 'iife',
-      name: pkg.name,
-    },
-    plugins: [
-      node({
-        browser: true,
-      }),
-      commonjs({
-        include: 'node_modules/**',
-        namedExports: {
-          'node_modules/fluture/index.js': ['isFuture', 'of', 'reject'],
-        },
-      }),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        presets: babelrc,
-      }),
-      uglify(),
-    ],
-  },
   {
     external: Object.keys(pkg.dependencies),
     input: 'src/index.js',
